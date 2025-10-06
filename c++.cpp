cfdb3e6a3f8f277a74486386bd5c91adcfd30c8e
millanhair31-cpu <millanhair31@gmail.com>
2025-10-06 08:45:50 -0500

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nombre;
+    cout << "Ingresa tu nombre: ";
+    cin >> nombre;
+    cout << "¡Bienvenido, " << nombre << "!" << endl;
+    return 0;
+}
